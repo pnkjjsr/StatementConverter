ae59c266141f5efa59fe8e50ff3e3c4ddd3fcf05
Pankaj Jasoria <pankajjasoria@gmail.com>
2025-09-17 06:17:04 +0000

    .wave-bg {       background-image: url("data:image/svg+xml,%3Csvg xmlns=

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { StatementConverter } from "@/components/StatementConverter";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background">
+    <div className="wave-bg flex min-h-screen w-full flex-col bg-background">
       <Header />
       <main className="flex flex-1 flex-col items-center justify-center p-4">
         <div className="w-full max-w-2xl">
