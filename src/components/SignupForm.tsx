b2a3200f3f114b7ab778329b54ce097d4154da50
Pankaj Jasoria <pankajjasoria@gmail.com>
2025-09-18 07:07:33 +0000

    use this UI for auth provider button, yahoo, facebook, google and X.com

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -34,7 +34,7 @@ interface SignupFormProps {
 const socialProviders = [
     { name: 'Google', icon: <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"><title>Google</title><path d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.05 1.05-2.36 1.67-4.02 1.67-3.27 0-5.92-2.68-5.92-5.99s2.65-5.99 5.92-5.99c1.7 0 3.02.62 4.08 1.62l2.6-2.6C16.8 3.3 14.7.98 12.48.98c-4.97 0-9 4.03-9 9s4.03 9 9 9c2.39 0 4.34-.79 5.82-2.26 1.5-1.5 2.22-3.75 2.22-6.14 0-.5-.06-.92-.15-1.33H12.48z"/></svg> },
     { name: 'Facebook', icon: <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 fill-current"><title>Facebook</title><path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.35C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.732 0 1.325-.593 1.325-1.325V1.325C24 .593 23.407 0 22.675 0z"/></svg> },
-    { name: 'Twitter', icon: <Twitter className="h-5 w-5" /> },
+    { name: 'Twitter', icon: <svg role="img" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5"><title>X</title><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 7.184L18.901 1.153Zm-1.65 19.57h2.61L7.129 2.65H4.43l12.822 18.074Z"/></svg> },
 ] as const;
 
 export function SignupForm({ onSwitchView }: SignupFormProps) {
@@ -187,15 +187,17 @@ export function SignupForm({ onSwitchView }: SignupFormProps) {
             </span>
             </div>
         </div>
-        <div className="grid grid-cols-3 gap-2">
+        <div className="space-y-2">
             {socialProviders.map((provider) => (
             <Button
                 key={provider.name}
                 variant="outline"
+                className="w-full"
                 onClick={() => handleOAuthLogin(provider.name.toLowerCase() as Provider)}
                 disabled={!!supabaseError}
             >
                 {provider.icon}
+                Continue with {provider.name}
             </Button>
             ))}
         </div>
