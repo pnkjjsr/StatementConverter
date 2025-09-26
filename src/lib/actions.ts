ba4e95afa4dbf717f91540a641e9079a72e29b4f
Pankaj Jasoria <pnkj_jsr@yahoo.co.in>
2025-09-26 15:15:25 +0530

    fix: add await in server side issue fix

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -24,7 +24,7 @@ async function getIpAddress(): Promise<string | null> {
 }
 
 async function getServerUser(): Promise<User | null> {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
